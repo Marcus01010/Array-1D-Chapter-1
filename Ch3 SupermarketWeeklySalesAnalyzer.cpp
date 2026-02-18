ca2cb1b9ab6faee9fdcd3280d96907c21e694489
Marcus Lewis <mlewis43@aum.edu>
2026-02-18 14:46:28 -0600

    Create Ch3 SupermarketWeeklySalesAnalyzer.cpp

---
+++ Ch3 SupermarketWeeklySalesAnalyzer.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+const int ITEMS = 5;
+const int COLS = 2;
+int cart[ITEMS][COLS];
+
+cout << "Enter price and quantity for 5 items:\n";
+
+for (int row = 0; row < ITEMS; row++)
+{
+cout << "Item " << (row + 1) << " price: ";
+cin >> cart[row][0];
+
+cout << "Item " << (row + 1) << " quantity: ";
+cin >> cart[row][1];
+}
+
+cout << "\nItem\tPrice\tQty\tSubtotal\n";
+
+int total = 0;
+
+for (int row = 0; row < ITEMS; row++)
+{
+int price = cart[row][0];
+int qty = cart[row][1];
+int subtotal = price * qty;
+
+cout << (row + 1) << "\t" << price << "\t" << qty << "\t" << subtotal << endl;
+
+total += subtotal;
+}
+
+cout << "------------------------------\n";
+cout << "Total:\t\t\t" << total << endl;
+
+return 0;
+}
